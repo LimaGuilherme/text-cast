3713c3b73ecd39b73e84f1a1123554440683b634
guilherme <guilherme@inceres.com.br>
2020-12-17 15:39:19 -0300

    Create web_app / API instance getter

---
+++ catalog/src/web_app.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_restful import Api
+
+config = None
+
+web_app = None
+api = None
+
+
+def get_web_app() -> Flask:
+    global web_app
+
+    if not web_app:
+        web_app = Flask(__name__)
+        web_app.config.from_object(config)
+
+    return web_app
+
+
+def get_api() -> Api:
+    global api
+
+    if not api:
+        api = Api(get_web_app())
+    return api
