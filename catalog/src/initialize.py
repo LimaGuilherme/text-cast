453791be82a0f743d18a952256d121c70948fd06
guilherme <guilherme@inceres.com.br>
2020-12-17 15:52:07 -0300

    Get Config instance

--- catalog/src/initialize.py
+++ catalog/src/initialize.py
@@ -1,4 +1,6 @@
 import web_app as web_app_module
+import configurations as config_module
 
 web_app = web_app_module.get_web_app()
 api = web_app_module.get_api()
+configurations = config_module.get_config()
