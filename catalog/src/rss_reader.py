daf15a117a53c5c8ff7a59a7ce1d87476649d113
guilherme <guilherme@inceres.com.br>
2020-12-20 11:09:30 -0300

    Implementing Rss reader and some corner cases.

---
+++ catalog/src/rss_reader.py
@@ -0,0 +1,45 @@
+import feedparser
+
+from catalog.src.entity import Episode
+from catalog.src.exceptions import InvalidRssUrl
+
+
+class RssReader:
+
+    def __init__(self, rss):
+        self.rss = rss
+
+    def read(self):
+        episodes = []
+        feed = feedparser.parse(self.rss)
+        self.__check_feed_status(feed)
+
+        for entry in feed.entries:
+            episodes.append(Episode(name=entry.title,
+                                    podcast=entry.author,
+                                    image=self.__scrap_image_entry(entry),
+                                    description=entry.summary,
+                                    audio=self.__scrap_audio_entry(entry.links)
+                                    )
+                            )
+        return episodes
+
+    def __check_feed_status(self, feed):
+        if feed.bozo:
+            bozo_exception = feed.bozo_exception.getMessage()
+            if bozo_exception == 'not well-formed (invalid token)':
+                raise InvalidRssUrl
+
+    def __scrap_image_entry(self, entry):
+        try:
+            return entry.image['href']
+        except AttributeError:
+            return None
+
+    def __scrap_audio_entry(self, entry_links):
+        for entry_link in entry_links:
+            if entry_link['type'] == 'audio/mpeg':
+                return entry_link['href']
+        return None
+
+
