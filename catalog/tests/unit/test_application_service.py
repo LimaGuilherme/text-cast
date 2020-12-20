a186639958bc1132d05ff10e9ffb52728118b0f4
guilherme <guilherme@inceres.com.br>
2020-12-19 22:47:18 -0300

    Create test for Episode Service and Rss Service.

--- catalog/tests/unit/test_application_service.py
+++ catalog/tests/unit/test_application_service.py
@@ -40,3 +40,39 @@ class TestPodcastService(TestCase):
 
         podcast_repository.list_all.assert_called()
         self.assertEqual(podcasts, podcasts_mock)
+
+
+class TestEpisodeService(TestCase):
+
+    def test_should_add_episode(self):
+        episode_repository = mock.MagicMock()
+        episode_service = EpisodeService(episode_repository)
+        episode_data = {
+            'name': 'ANDRÉ VIANCO - Flow Podcast #279',
+            'podcast': 'Flow Podcast',
+            'image': 'https://artworks-flow.s3-sa-east-1.amazonaws.com/279_Andre_Vianco.jpg',
+            'description': 'André Vianco é um escritor brabo que escreveu os livros que mais marcaram a vida do Monark',
+            'audio': 'https://content.blubrry.com/flowpdc/279_ANDRE_VIANCO.mp3'
+        }
+        episode_service.create(episode_data)
+        episode_repository.add.assert_called()
+
+        episode = episode_repository.add.mock_calls[0].args[0]
+        self.assertEqual(episode.name, 'ANDRÉ VIANCO - Flow Podcast #279')
+        self.assertEqual(episode.podcast, 'Flow Podcast')
+        self.assertEqual(episode.image, 'https://artworks-flow.s3-sa-east-1.amazonaws.com/279_Andre_Vianco.jpg')
+        self.assertEqual(episode.description, 'André Vianco é um escritor brabo que escreveu os livros que mais marcaram a vida do Monark')
+        self.assertEqual(episode.audio, 'https://content.blubrry.com/flowpdc/279_ANDRE_VIANCO.mp3')
+
+
+class TestRssReaderTest(TestCase):
+
+    def test_should_read_rss_from_podcast(self):
+        podcast = Podcast(name='Flow Podcast',
+                          rss='https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss')
+
+        rss_service = RssReaderService(podcast)
+        rss_service.read()
+
+        self.assertIsInstance(rss_service, RssReaderService)
+        self.assertEqual(rss_service.item, 'DontKnow')
