429e0195b15661f9dad6efcc65a01692fc7252fc
guilherme <guilherme@inceres.com.br>
2020-12-21 12:07:36 -0300

    Fix Rss Reader tests.

--- catalog/tests/integration/test_rss_reader.py
+++ catalog/tests/integration/test_rss_reader.py
@@ -12,8 +12,8 @@ class Test(TestCase):
 
     def test_should_raise_when_given_a_invalid_rss_url(self):
         with self.assertRaises(InvalidRssUrl):
-            RssReader().read("https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss")
+            RssReader().read("https://rss-feed-flowpodcast-2eqj3-ue.a.run.app/feed/rss")
 
     def test_should_read_b9_rss_and_return_a_list_of_episodes(self):
-        episodes = RssReader().read("https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss")
+        episodes = RssReader().read("https://feeds.simplecast.com/AZQtlzze")
         self.assertGreaterEqual(len(episodes), 346)
