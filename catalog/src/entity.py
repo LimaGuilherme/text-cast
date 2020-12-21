d605a29573bb53299470f63531a20067d347e718
guilherme <guilherme@inceres.com.br>
2020-12-21 11:41:02 -0300

    Entities now has ids

--- catalog/src/entity.py
+++ catalog/src/entity.py
@@ -1,13 +1,15 @@
 class Podcast:
 
-    def __init__(self, name: str, rss: str) -> None:
+    def __init__(self, name: str, rss: str, _id: str = None) -> None:
+        self.id = _id
         self.name = name
         self.rss = rss
 
 
 class Episode:
 
-    def __init__(self, name: str, podcast: str, image: str, description: str, audio: str) -> None:
+    def __init__(self, name: str, podcast: str, image: str, description: str, audio: str, _id: str = None) -> None:
+        self.id = _id
         self.name = name
         self.podcast = podcast
         self.image = image
