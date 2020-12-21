2bd15a7256224699e6e30c617df0b34218857960
guilherme <guilherme@inceres.com.br>
2020-12-21 11:40:38 -0300

    update rss reader test.

--- catalog/tests/integration/test_rss_reader.py
+++ catalog/tests/integration/test_rss_reader.py
@@ -7,16 +7,13 @@ from catalog.src.rss_reader import RssReader
 class Test(TestCase):
 
     def test_should_read_flow_rss_and_return_a_list_of_episodes(self):
-        rss = RssReader("https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss")
-        episodes = rss.read()
+        episodes = RssReader().read("https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss")
         self.assertGreaterEqual(len(episodes), 305)
 
     def test_should_raise_when_given_a_invalid_rss_url(self):
         with self.assertRaises(InvalidRssUrl):
-            rss = RssReader("https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rs")
-            rss.read()
+            RssReader().read("https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss")
 
     def test_should_read_b9_rss_and_return_a_list_of_episodes(self):
-        rss = RssReader("https://feeds.simplecast.com/AZQtlzze")
-        episodes = rss.read()
+        episodes = RssReader().read("https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss")
         self.assertGreaterEqual(len(episodes), 346)
