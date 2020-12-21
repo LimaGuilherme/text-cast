f73db4f4e5a2db8a93b1be7f2ff8b8635e6ed0c6
guilherme <guilherme@inceres.com.br>
2020-12-21 14:06:06 -0300

    Fix application service unit test

--- catalog/tests/unit/test_application_service.py
+++ catalog/tests/unit/test_application_service.py
@@ -1,45 +1,55 @@
 from unittest import TestCase, mock
 
+from catalog.src.application_service import PodcastService, EpisodeService
+
 
 class TestPodcastService(TestCase):
 
-    def test_should_add_podcast(self):
+    def test_create_podcast(self):
         podcast_repository = mock.MagicMock()
         podcast_service = PodcastService(podcast_repository)
         podcast_data = {
             'name': 'Flow Podcast',
             'rss': 'https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss'
         }
-        podcast_service.create(podcast_data)
+        podcast_service.create_podcast(podcast_data)
         podcast_repository.add.assert_called()
 
         podcast = podcast_repository.add.mock_calls[0].args[0]
         self.assertEqual(podcast.name, 'Flow Podcast')
         self.assertEqual(podcast.rss, 'https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss')
 
-    def test_should_return_one_podcast(self):
-        podcast_repository = mock.MagicMock()
-        podcast_mock = mock.MagicMock()
-
-        podcast_service = PodcastService(podcast_repository)
-        podcast_repository.return_value = podcast_mock
-
-        podcast = podcast_service.get_podcast('Flow Podcast')
-
-        podcast_repository.get_by_name.assert_called()
-        self.assertEqual(podcast, podcast_mock)
-
-    def test_should_return_all_podcasts(self):
-        podcast_repository = mock.MagicMock()
+    def test_list_podcasts(self):
         podcasts_mock = mock.MagicMock()
+        repository_mock = mock.MagicMock()
+        repository_mock.list_all.return_value = podcasts_mock
 
-        podcast_service = PodcastService(podcast_repository)
-        podcast_repository.return_value = podcasts_mock
+        service = PodcastService(repository_mock)
+        podcasts = service.list_podcasts()
 
-        podcasts = podcast_service.list_all()
+        repository_mock.list_all.assert_called()
+        self.assertEqual(podcasts_mock, podcasts)
 
-        podcast_repository.list_all.assert_called()
-        self.assertEqual(podcasts, podcasts_mock)
+    def test_get_podcast(self):
+        repository = mock.MagicMock()
+        podcast_mock = mock.MagicMock()
+        repository.list.return_value = podcast_mock
+
+        podcast_service = PodcastService(repository)
+        podcast = podcast_service.get_podcast('1')
+
+        repository.list.assert_called()
+        self.assertEqual(podcast_mock, podcast)
+
+    def test_remove_product(self):
+        podcast_mock = mock.MagicMock()
+        repository = mock.MagicMock()
+
+        repository.list.return_value = podcast_mock
+
+        service = PodcastService(repository)
+        service.remove_podcast('1')
+        repository.delete.assert_called_with('1')
 
 
 class TestEpisodeService(TestCase):
@@ -54,7 +64,7 @@ class TestEpisodeService(TestCase):
             'description': 'André Vianco é um escritor brabo que escreveu os livros que mais marcaram a vida do Monark',
             'audio': 'https://content.blubrry.com/flowpdc/279_ANDRE_VIANCO.mp3'
         }
-        episode_service.create(episode_data)
+        episode_service.create_episode(episode_data)
         episode_repository.add.assert_called()
 
         episode = episode_repository.add.mock_calls[0].args[0]
@@ -64,15 +74,34 @@ class TestEpisodeService(TestCase):
         self.assertEqual(episode.description, 'André Vianco é um escritor brabo que escreveu os livros que mais marcaram a vida do Monark')
         self.assertEqual(episode.audio, 'https://content.blubrry.com/flowpdc/279_ANDRE_VIANCO.mp3')
 
+    def test_list_episodes(self):
+        episodes_mock = mock.MagicMock()
+        repository_mock = mock.MagicMock()
+        repository_mock.list_all.return_value = episodes_mock
 
-class TestRssReaderTest(TestCase):
+        service = EpisodeService(repository_mock)
+        episodes = service.list_episodes()
 
-    def test_should_read_rss_from_podcast(self):
-        podcast = Podcast(name='Flow Podcast',
-                          rss='https://rss-feed-flowpodcast-2eqj3fl3la-ue.a.run.app/feed/rss')
+        repository_mock.list_all.assert_called()
+        self.assertEqual(episodes_mock, episodes)
 
-        rss_service = RssReaderService(podcast)
-        rss_service.read()
+    def test_get_podcast(self):
+        repository_mock = mock.MagicMock()
+        episode_mock = mock.MagicMock()
+        repository_mock.list.return_value = episode_mock
 
-        self.assertIsInstance(rss_service, RssReaderService)
-        self.assertEqual(rss_service.item, 'DontKnow')
+        service = EpisodeService(repository_mock)
+        episode = service.get_episode('1')
+
+        repository_mock.list.assert_called()
+        self.assertEqual(episode_mock, episode)
+
+    def test_remove_product(self):
+        episode_mock = mock.MagicMock()
+        repository_mock = mock.MagicMock()
+
+        repository_mock.list.return_value = episode_mock
+
+        service = EpisodeService(repository_mock)
+        service.remove_episode('1')
+        repository_mock.delete.assert_called_with('1')
