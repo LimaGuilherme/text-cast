edb5a47a9ee1d3915e2759f868e6a58b0f5567e9
guilherme <guilherme@inceres.com.br>
2020-12-21 14:06:30 -0300

    Fix configurations test with new varenv.

--- catalog/tests/unit/test_configurations.py
+++ catalog/tests/unit/test_configurations.py
@@ -8,16 +8,26 @@ class TestConfig(TestCase):
     def test_should_init(self):
         configurations = Config(
             MONGO_HOST='local',
-            MONGO_PORT='6669'
+            MONGO_PORT='6669',
+            MONGO_DB='textcast',
+            MONGO_PODCAST_COLLECTION='podcasts',
+            MONGO_EPISODE_COLLECTION='episodes',
+
         )
         self.assertEqual(configurations.MONGO_HOST, 'local')
         self.assertEqual(configurations.MONGO_PORT, '6669')
+        self.assertEqual(configurations.MONGO_DB, 'textcast')
+        self.assertEqual(configurations.MONGO_PODCAST_COLLECTION, 'podcasts')
+        self.assertEqual(configurations.MONGO_EPISODE_COLLECTION, 'episodes')
 
     @mock.patch('catalog.src.configurations.os')
     def test_should_get_configurations(self, os_mock):
         os_mock.environ = {
             'MONGO_HOST': 'mock-host',
-            'MONGO_PORT': 'mock-port'
+            'MONGO_PORT': 'mock-port',
+            'MONGO_DB': 'mock-db',
+            'MONGO_PODCAST_COLLECTION': 'mock-podcast-collection',
+            'MONGO_EPISODE_COLLECTION': 'mock-episode-collection'
         }
 
         configurations = get_config()
