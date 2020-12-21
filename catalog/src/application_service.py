380d369d42dd8daa4254936855aebabe2ff1fa48
guilherme <guilherme@inceres.com.br>
2020-12-21 11:46:55 -0300

    Create application service methods.

---
+++ catalog/src/application_service.py
@@ -0,0 +1,41 @@
+from typing import List
+
+from catalog.src.entity import Podcast, Episode
+
+
+class PodcastService:
+
+    def __init__(self, repository) -> None:
+        self.__repository = repository
+
+    def create_podcast(self, podcast_data: dict) -> Podcast:
+        podcast = Podcast(**podcast_data)
+        return self.__repository.add(podcast)
+
+    def get_podcast(self, podcast_id: str) -> Podcast:
+        return self.__repository.list(podcast_id)
+
+    def list_podcasts(self) -> List[Podcast]:
+        return self.__repository.list_all()
+
+    def remove_podcast(self, podcast_id: str) -> None:
+        self.__repository.remove(podcast_id)
+
+
+class EpisodeService:
+
+    def __init__(self, repository) -> None:
+        self.__repository = repository
+
+    def create_episode(self, episode_data: dict) -> Episode:
+        episode = Episode(**episode_data)
+        return self.__repository.add(episode)
+
+    def get_episode(self, episode_id: str) -> Episode:
+        return self.__repository.list(episode_id)
+
+    def list_episodes(self) -> List[Episode]:
+        return self.__repository.list_all()
+
+    def remove_episode(self, episode_id: str) -> None:
+        self.__repository.remove(episode_id)
