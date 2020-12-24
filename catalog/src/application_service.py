33c21d964a6924d5c784b8592415aceb70f77489
guilherme <guilherme@inceres.com.br>
2020-12-24 16:36:51 -0300

    Remove episode Service, no longer needed

--- catalog/src/application_service.py
+++ catalog/src/application_service.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from catalog.src.entity import Podcast, Episode
+from catalog.src.entity import Podcast
 
 
 class PodcastService:
@@ -12,30 +12,12 @@ class PodcastService:
         podcast = Podcast(**podcast_data)
         return self.__repository.add(podcast)
 
-    def get_podcast(self, podcast_id: str) -> Podcast:
-        return self.__repository.list(podcast_id)
+    def list_episodes(self, podcast_id: str) -> Podcast:
+        podcast = self.__repository.list(podcast_id)
+        return podcast
 
     def list_podcasts(self) -> List[Podcast]:
         return self.__repository.list_all()
 
     def remove_podcast(self, podcast_id: str) -> None:
         self.__repository.delete(podcast_id)
-
-
-class EpisodeService:
-
-    def __init__(self, repository) -> None:
-        self.__repository = repository
-
-    def create_episode(self, episode_data: dict) -> Episode:
-        episode = Episode(**episode_data)
-        return self.__repository.add(episode)
-
-    def get_episode(self, episode_id: str) -> Episode:
-        return self.__repository.list(episode_id)
-
-    def list_episodes(self) -> List[Episode]:
-        return self.__repository.list_all()
-
-    def remove_episode(self, episode_id: str) -> None:
-        self.__repository.delete(episode_id)
