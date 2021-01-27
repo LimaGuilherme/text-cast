38cd7a112fac81cd3ff85b0a141fb3530bd12673
guilherme <guilherme@inceres.com.br>
2021-01-27 11:14:33 -0300

    implementing caching layer.

--- catalog/src/application_service.py
+++ catalog/src/application_service.py
@@ -5,12 +5,13 @@ from catalog.src.entity import Podcast
 
 class PodcastService:
 
-    def __init__(self, repository, rss_reader) -> None:
+    def __init__(self, repository, rss_reader, cache_repository) -> None:
         self.__repository = repository
         self.__rss_reader = rss_reader
+        self.__cache_repository = cache_repository
 
     def create_podcast(self, podcast_data: dict) -> Podcast:
-        podcast_infos = self.__rss_reader.read()
+        podcast_infos = self.__rss_reader.read_podcast(podcast_data['rss'])
         podcast = Podcast(
             rss=podcast_data['rss'],
             name=podcast_infos['name'],
@@ -18,8 +19,15 @@ class PodcastService:
         return self.__repository.add(podcast)
 
     def list_episodes(self, podcast_id: str) -> Podcast:
+        episodes = self.__cache_repository.get(podcast_id)
         podcast = self.__repository.list(podcast_id)
+
+        if episodes:
+            podcast.associate_episodes(episodes)
+            return podcast
+
         episodes = self.__rss_reader.read_episodes(podcast.rss)
+        self.__cache_repository.set(podcast_id, episodes)
         podcast.associate_episodes(episodes)
         return podcast
 
