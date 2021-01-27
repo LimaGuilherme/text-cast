5cdea4e4e59685d3fa4c5cd5a1b99f9db6e045da
guilherme <guilherme@inceres.com.br>
2021-01-27 11:15:15 -0300

    pass cache repository as dependency

--- catalog/src/initialize.py
+++ catalog/src/initialize.py
@@ -3,17 +3,19 @@ from catalog.src import web_app as web_app_module
 from catalog.src import configurations as config_module
 from catalog.src import endpoints
 from catalog.src.application_service import PodcastService
-from catalog.src.repositories import create_repository
+from catalog.src.infrascructure.cache import CacheRepository
+from catalog.src.infrascructure.mongodb_repository import create_repository
 from catalog.src.rss_reader import RssReader
 
 web_app = web_app_module.get_web_app()
 api = web_app_module.get_api()
 configurations = config_module.get_config()
 
+cache_repository = CacheRepository()
 rss_reader = RssReader()
 mongodb_repository = create_repository(configurations)
 
-podcast_service = PodcastService(mongodb_repository, rss_reader)
+podcast_service = PodcastService(mongodb_repository, rss_reader, cache_repository)
 
 
 endpoints.register(
